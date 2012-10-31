ba261547aee73e5f1be78d5258fe6e7d714389ec
Tom Smith <tom@takeontom.com>
2012-10-31 02:49:43 +0000

    ref #63 completed basic dev environment setup script

--- cookbooks/protec-redmine/recipes/default.rb
+++ cookbooks/protec-redmine/recipes/default.rb
@@ -1,7 +1,15 @@
 include_recipe "apt"
 include_recipe "mysql"
-include_recipe "ruby"
-include_recipe "nginx"
+include_recipe "mysql::server"
+#include_recipe "nginx"
+
+package "ruby1.8" do
+    :install
+end
+
+package "ruby1.8-dev" do
+    :install
+end
 
 package "rubygems1.8" do
     :install
@@ -31,45 +39,12 @@ package "libmagickwand-dev" do
     :install
 end
 
-package "thin" do
-    :install
-end
+#package "thin" do
+#    :install
+#end
 
-gem_package "rails" do
-    action :install
-end
-
-gem_package "mysql" do
-    action :install
-end
-
-gem_package "mysql2" do
-    action :install
-end
-
-gem_package "bundler" do
-    action :install
-end
-
-gem_package "polyglot" do
-    action :install
-end
-
-execute "bundle" do
-    command "bundle install --without development test postgresql sqlite"
-    cwd "#{node['redmine_dir']}"
-    action :run
-end
-
-execute "session_store" do
-    command "rake generate_session_store"
-    cwd "#{node['redmine_dir']}"
-    action :run
-end
-
-execute "secret_token" do
-    command "rake generate_secret_token"
-    cwd "#{node['redmine_dir']}"
+execute "gem_bundler" do
+    command "gem install bundler"
     action :run
 end
 
@@ -89,5 +64,52 @@ directory "#{node['redmine_dir']}/files" do
     action :create
     owner "www-data"
     group "www-data"
+end
+
+template "#{node['redmine_dir']}/config/database.yml" do
+    source "database.yml.erb"
+end
+
+execute "bundle" do
+    command "bundle install --without development test postgresql sqlite"
+    cwd "#{node['redmine_dir']}"
+    action :run
+end
+
+execute "session_store" do
+    command "rake generate_session_store"
+    cwd "#{node['redmine_dir']}"
+    action :run
+end
+
+execute "create_database" do
+    command "mysql -u root -p#{node['mysql']['server_root_password']} -e 'CREATE DATABASE #{node['database']['schema']} CHARACTER SET utf8;'"
+    action :run
+end
+
+execute "setup_schema" do
+    command "rake db:migrate"
+    environment ({'RAILS_ENV' => 'production'})
+    cwd "#{node['redmine_dir']}"
+    action :run
+end
+
+execute "default_data" do
+    command "rake redmine:load_default_data"
+    environment ({'RAILS_ENV' => 'production', 'REDMINE_LANG' => 'en'})
+    cwd "#{node['redmine_dir']}"
+    action :run
+end
+
+execute "secret_token" do
+    command "rake generate_secret_token"
+    cwd "#{node['redmine_dir']}"
+    action :run
+end
+
+execute "start_webrick" do
+    command "ruby script/rails server webrick -e production -p 80 -d"
+    cwd "#{node['redmine_dir']}"
+    action :run
 end
 
